26666626f6fd2e294512bcab8977f74a12206d6a
Javier Suárez <javiersuarezruiz@hotmail.com>
2021-07-03 19:21:12 +0200

    Add Converters samples

---
+++ Converters/.NET MAUI/Converters/Converters/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace Converters
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Converters/Xamarin.Forms/Converters/Converters/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Converters
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
