3d1b3b4acb529cd173849fe93cf38746fd932707
Javier Suárez <javiersuarezruiz@hotmail.com>
2021-07-04 12:00:29 +0200

    Add Triggers samples

---
+++ Triggers/.NET MAUI/Triggers/Triggers/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace Triggers
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate<Startup>
+	{
+	}
+}
---
+++ Triggers/.NET MAUI/Triggers/Triggers/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace Triggers
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
