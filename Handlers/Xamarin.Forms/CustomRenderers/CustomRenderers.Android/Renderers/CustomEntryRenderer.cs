fdf18e03a38cb2d69c52a821a04ae3dd1d3ab41d
Javier Suárez <javiersuarezruiz@hotmail.com>
2021-07-11 19:26:51 +0200

    Added Renderers Xamarin.Forms sample

---
+++ Handlers/Xamarin.Forms/CustomRenderers/CustomRenderers.Android/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,92 @@
+﻿using Android.Content;
+using Android.Widget;
+using CustomRenderers;
+using CustomRenderers.Droid.Renderers;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace CustomRenderers.Droid.Renderers
+{
+    public class CustomEntryRenderer : ViewRenderer<CustomEntry, EditText>
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<CustomEntry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null)
+            {
+                SetNativeControl(new EditText(Context));
+            }
+
+            UpdateText();
+            UpdateTextColor();
+            UpdatePlaceholder();
+            UpdatePlaceholderColor();
+            UpdateCharacterSpacing();
+            UpdateHorizontalTextAlignment();
+            UpdateVerticalTextAlignment();
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == CustomEntry.TextProperty.PropertyName)
+                UpdateText();
+            else if (e.PropertyName == CustomEntry.TextColorProperty.PropertyName)
+                UpdateTextColor();
+            if (e.PropertyName == CustomEntry.PlaceholderProperty.PropertyName)
+                UpdatePlaceholder();
+            else if (e.PropertyName == Entry.PlaceholderColorProperty.PropertyName)
+                UpdatePlaceholderColor();
+            else if (e.PropertyName == Entry.CharacterSpacingProperty.PropertyName)
+                UpdateCharacterSpacing();
+            else if (e.PropertyName == Entry.HorizontalTextAlignmentProperty.PropertyName)
+                UpdateHorizontalTextAlignment();
+            else if (e.PropertyName == Entry.VerticalTextAlignmentProperty.PropertyName)
+                UpdateVerticalTextAlignment();
+
+            base.OnElementPropertyChanged(sender, e);
+        }
+
+        void UpdateText()
+        {
+
+        }
+
+        void UpdateTextColor()
+        {
+
+        }
+
+        void UpdatePlaceholder()
+        {
+
+        }
+
+        void UpdatePlaceholderColor()
+        {
+
+        }
+
+        void UpdateCharacterSpacing()
+        {
+
+        }
+
+        void UpdateHorizontalTextAlignment()
+        {
+
+        }
+
+        void UpdateVerticalTextAlignment()
+        {
+
+        }
+    }
+}
---
+++ Handlers/Xamarin.Forms/CustomRenderers/CustomRenderers/CustomEntry.cs
@@ -0,0 +1,69 @@
+﻿using Xamarin.Forms;
+
+namespace CustomRenderers
+{
+    public class CustomEntry : View
+    {
+        public static readonly BindableProperty TextProperty =
+            BindableProperty.Create(nameof(Text), typeof(string), typeof(Entry), string.Empty);
+
+        public static readonly BindableProperty TextColorProperty =
+            BindableProperty.Create(nameof(TextColor), typeof(Color), typeof(Entry), Color.Default);
+
+        public static readonly BindableProperty PlaceholderProperty =
+            BindableProperty.Create(nameof(Placeholder), typeof(string), typeof(Entry), string.Empty);
+
+        public static readonly BindableProperty PlaceholderColorProperty =
+            BindableProperty.Create(nameof(PlaceholderColor), typeof(Color), typeof(Entry), Color.Default);
+
+        public static readonly BindableProperty CharacterSpacingProperty =
+            BindableProperty.Create(nameof(CharacterSpacing), typeof(double), typeof(Entry), 0.0d);
+
+        public static readonly BindableProperty HorizontalTextAlignmentProperty =
+            BindableProperty.Create(nameof(HorizontalTextAlignment), typeof(string), typeof(Entry), string.Empty);
+
+        public static readonly BindableProperty VerticalTextAlignmentProperty =
+            BindableProperty.Create(nameof(VerticalTextAlignment), typeof(string), typeof(Entry), string.Empty);
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public Color TextColor
+        {
+            get { return (Color)GetValue(TextColorProperty); }
+            set { SetValue(TextColorProperty, value); }
+        }
+        public string Placeholder
+        {
+            get { return (string)GetValue(PlaceholderProperty); }
+            set { SetValue(PlaceholderProperty, value); }
+        }
+
+        public Color PlaceholderColor
+        {
+            get { return (Color)GetValue(PlaceholderColorProperty); }
+            set { SetValue(PlaceholderColorProperty, value); }
+        }
+
+        public double CharacterSpacing
+        {
+            get { return (double)GetValue(CharacterSpacingProperty); }
+            set { SetValue(CharacterSpacingProperty, value); }
+        }
+
+        public TextAlignment HorizontalTextAlignment
+        {
+            get { return (TextAlignment)GetValue(HorizontalTextAlignmentProperty); }
+            set { SetValue(HorizontalTextAlignmentProperty, value); }
+        }
+
+        public TextAlignment VerticalTextAlignment
+        {
+            get { return (TextAlignment)GetValue(VerticalTextAlignmentProperty); }
+            set { SetValue(VerticalTextAlignmentProperty, value); }
+        }
+    }
+}
