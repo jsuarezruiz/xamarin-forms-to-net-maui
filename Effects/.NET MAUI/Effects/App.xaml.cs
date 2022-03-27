3e762a6f664b2cbd14c430d139ed69eac1ce46f7
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2022-03-27 17:50:25 +0200

    Updated .NET MAUI Effects sample

---
+++ Effects/.NET MAUI/Effects/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Effects;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ Effects/.NET MAUI/Effects/MauiProgram.cs
@@ -0,0 +1,24 @@
+﻿using Effects.Effects;
+using Microsoft.Maui.Controls.Hosting;
+
+namespace Effects;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureEffects(effects =>
+            {
+                effects.Add(typeof(FocusRoutingEffect), typeof(PlatformFocusPlatformEffect));
+            })
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+        return builder.Build();
+    }
+}
