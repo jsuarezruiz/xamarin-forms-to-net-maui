0e63e699e466edf87e1e896d099b32a1a59e22d5
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2022-03-27 13:46:42 +0200

    Updated .NET MAUI Converters sample

---
+++ Converters/.NET MAUI/Converters/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Converters;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ Converters/.NET MAUI/Converters/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Converters;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
