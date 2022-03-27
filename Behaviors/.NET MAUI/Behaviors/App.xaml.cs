4289dc6108460a5df318a020ae6d7ef22115eebe
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2022-03-27 13:25:13 +0200

    Updated Behaviors .NET MAUI sample

---
+++ Behaviors/.NET MAUI/Behaviors/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Behaviors;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ Behaviors/.NET MAUI/Behaviors/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Behaviors;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
